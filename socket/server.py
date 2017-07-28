70cc4e8a3da07f5e7eaebca0f6d0305a352b2113
root@nilswang-Aspire-E5-573G <nils2012629@hotmail.com>
2017-07-28 10:51:05 +0200

    new server

--- socket/server.py
+++ socket/server.py
@@ -25,7 +25,6 @@ if __name__ == "__main__":
     PORT = 5000
      
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this has no effect, why ?
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(("0.0.0.0", PORT))
     server_socket.listen(10)
